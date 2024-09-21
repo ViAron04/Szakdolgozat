483e0d8482dda573e67ec6542a4bb22333ac487f
ViAron04 <viragharon04@gmail.com>
2024-09-21 16:04:44 +0200

    FirstCommit
    Remélem, működni fogsz

---
+++ HungaryGo/app/src/main/java/com/example/HungaryGo/MainActivity.kt
@@ -0,0 +1,227 @@
+package com.example.HungaryGo
+
+import android.Manifest
+import android.animation.ValueAnimator
+import android.content.pm.PackageManager
+import android.location.Location
+import com.google.android.gms.location.LocationRequest
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.animation.LinearInterpolator
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+//lássuk, mukodik-e a git
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    var mGoogleMap: GoogleMap? = null
+
+    private val FINE_PERMISSION_CODE = 1
+    private lateinit var currentLocation: Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback1: LocationCallback
+
+    val db = FirebaseDatabase.getInstance()
+    val markerLocations: MutableMap<String? ,MarkerOptions> = mutableMapOf()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this) //segít energiatakarékosan és hatékonyan megszerezni a helyadatokat
+        getCurrentLocationUser()
+
+        var currentMarker: Marker? = null
+
+        locationCallback1 = object : LocationCallback() {
+
+            override fun onLocationResult(locationResult: LocationResult) { //akkor hívódik meg, ha új helymeghatározási eredmények érkeznek
+                super.onLocationResult(locationResult)
+                val location = locationResult.lastLocation //kiszűri a legutóbbi pozíciót
+
+                getLocationPacks() //Firebase-ben tárolt helyeket hívja le és jeleníti meg
+
+                if (location != null) {
+                    currentMarker?.remove()
+
+                    currentLocation = location
+                    val latLng = LatLng(currentLocation.latitude, currentLocation.longitude)
+                    val icon = BitmapDescriptorFactory.fromResource(R.drawable.szemuveges_atmeneti)
+
+                    currentMarker = mGoogleMap?.addMarker(MarkerOptions().position(latLng).title("Szerénységem").icon(icon))!!
+
+                    val newLatLng = LatLng(location.latitude, location.longitude)
+                    mGoogleMap?.animateCamera(CameraUpdateFactory.newLatLng(newLatLng)) //a kamera ezáltal követi a felhasználót
+                }
+            }
+        }
+    }
+
+
+    private fun getLocationPacks()
+    {
+        //belép a location packsba
+        val locationPacksRef = db.getReference("location packs")
+
+        locationPacksRef.addListenerForSingleValueEvent(object : ValueEventListener { //csak egyszer kéri el az adatokat az adatbázisból
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                if (snapshot.exists()) { //létezik-e az adat
+
+                    for (buildingSnapshot1 in snapshot.children) { //Pl. Pannon Egyetem, Vasszécseny kör
+                        val locationPack = buildingSnapshot1
+                            for (buildingSnapshot in locationPack.children) {
+                                val buildingName =
+                                    buildingSnapshot.key  //Pl. "A épület", "I épület"
+                                if (!markerLocations.containsKey(buildingName)) {
+                                    val buildingMap = buildingSnapshot.value as Map<String, Any>
+
+                                    val latitude = buildingMap["latitude"] as Double
+                                    val longitude = buildingMap["longitude"] as Double
+
+                                    // Lehelyezi/ hozzáadja a Map-hez a markereket
+                                    val actualMarker: MarkerOptions = MarkerOptions()
+                                        .position(LatLng(latitude, longitude))
+                                        .title(buildingName)
+                                        .draggable(false)
+
+                                    markerLocations[buildingName] = actualMarker
+
+                                    mGoogleMap?.addMarker(actualMarker)
+                                }
+                            }
+                    }
+                } else {
+                    println("Nem találtam helyszíneket az adatbázisban")
+                }
+            }
+            override fun onCancelled(error: DatabaseError) { //hibakezelés
+                println("Error getting data: ${error.message}")
+            }
+        })
+
+    }
+
+    //meghatározza  a jelenlegi pozíciót
+    private fun getCurrentLocationUser() {
+        //engedély ellenőrzése, haa nincsenek meg, engedélyt kér
+        if (ActivityCompat.checkSelfPermission(
+                this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), FINE_PERMISSION_CODE)
+            return
+        }
+
+        //lekéri a felhasználó tartózkodási helyét
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            if (location != null) {
+                currentLocation = location
+                //kiírja a koordinátákat, ha elérhető lokáció
+                Toast.makeText(applicationContext, "${currentLocation.latitude}, ${currentLocation.longitude}",
+                    Toast.LENGTH_LONG).show()
+
+                val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFelulet) as SupportMapFragment
+                mapFragment.getMapAsync(this)
+            }
+        }
+    }
+
+    //ha a felugró ablakban nem engedélyezzük a felhasználást, akkor kiirja alanti üzenetet
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            FINE_PERMISSION_CODE -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocationUser()
+            } else {
+                Toast.makeText(this, "Kérem engedélyezze a helymegosztást!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    //akkor hívódik meg, ha működik az API
+    override fun onMapReady(googleMap: GoogleMap) {
+        mGoogleMap = googleMap
+
+        //Tedd választhatóvá!!
+        //mGoogleMap?.mapType = GoogleMap.MAP_TYPE_SATELLITE
+
+        val latLng = LatLng(currentLocation.latitude, currentLocation.longitude)
+
+        // milyen gyakran és minőségben érkezzenek helyadatok?
+        locationRequest = LocationRequest.create().apply {
+            interval = 1000 //régebbi, érdrmes lecserélni
+            fastestInterval = 1000
+            //priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+           // setSmallestDisplacement(1f) //csak akkor változik a pozíció, ha egy métert haladok
+
+        }
+        Priority.PRIORITY_HIGH_ACCURACY
+
+        // ellenőrzés
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                //locationRequest megszerzi a helyadatokat, locationCallbacknek továbbküldi
+                    fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback1, null)
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), FINE_PERMISSION_CODE)
+            }
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+            Toast.makeText(this, "Kérem engedélyezze a helymegosztást!", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    /*
+    //megszünteti a helyfrissítések kérését, amikor az Activity háttérbe kerül (tehát amikor bezáródik)
+    override fun onPause() {
+        super.onPause()
+        fusedLocationClient.removeLocationUpdates(locationCallback1)
+    }*/
+
+    //ráközelít a helyzetemre
+    fun goToMe(view: View) {
+        val latLng = LatLng(currentLocation.latitude, currentLocation.longitude)
+        mGoogleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18f))
+    }
+
+    //Jelenleg nincs használatba, haladás folyamatosabb megjelenítését segítené
+    private fun animateMarkerToPosition(marker: Marker, finalPosition: LatLng) {
+        val startPosition = marker.position
+        val valueAnimator = ValueAnimator.ofFloat(0f, 1f)
+        valueAnimator.duration = 1000
+        valueAnimator.interpolator = LinearInterpolator()
+
+        valueAnimator.addUpdateListener { animation ->
+            val v = animation.animatedValue as Float
+            val lat = v * finalPosition.latitude + (1 - v) * startPosition.latitude
+            val lng = v * finalPosition.longitude + (1 - v) * startPosition.longitude
+            val newPosition = LatLng(lat, lng)
+            marker.position = newPosition // Update marker position
+        }
+        valueAnimator.start()
+    }
+
+}
